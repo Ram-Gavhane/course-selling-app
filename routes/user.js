52063b2c05f5826a7cf7c22ce2bcf0fa41a9fbb1
Ram Gavhane <ramgavhane28@gmail.com>
2025-02-16 19:08:40 +0530

    routes skeleton added

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,23 @@ const userRouter = Router();
 
 userRouter.post("/signup", function(req, res){
     
-})
+});
+
+userRouter.post("/login", function(req, res){
+
+});
+
+userRouter.get("/allCourses", function(req, res){
+
+});
+
+userRouter.post("/buyCourse", function(req, res){
+
+});
+
+userRouter.get("/purchasedCourses", function(req, res){
+
+});
 
 module.exports = {
     userRouter
